314f75995222f370337c8167b8cca8d5b4bdae61
Ben Force <xdd741852963@gmail.com>
2022-06-26 20:59:10 +0800

    add moralis event listener

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import Header from "../components/Header"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>NFT Marketplace</title>
+                <meta name="description" content="NFT Marketplace" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider
+                appId={process.env.NEXT_PUBLIC_APP_ID}
+                serverUrl={process.env.NEXT_PUBLIC_SERVER_URL}
+            >
+                <Header />
+                <Component {...pageProps} />
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
