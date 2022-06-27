c46188e9669f89c552c4dd820faac9675738b18e
Ben Force <xdd741852963@gmail.com>
2022-06-27 17:04:42 +0800

    add abi and NFTBox

---
+++ components/NFTBox.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from "react"
+import { useWeb3Contract, useMoralis } from "react-moralis"
+import nftMarketplaceAbi from "../constants/NftMarketplace.json"
+import nftAbi from "../constants/BasicNft.json"
+import Image from "next/image"
+import { Card } from "web3uikit"
+import { ethers } from "ethers"
+
+const truncateStr = (fullStr, strLen) => {
+    if (fullStr <= strLen) return fullStr
+
+    const separator = "..."
+    const separatorLength = separator.length
+    const charsToShow = strLen - separatorLength
+    const frontChars = Math.ceil(charsToShow / 2)
+    const backChars = Math.floor(charsToShow / 2)
+    return (
+        fullStr.substring(0, frontChars) +
+        separator +
+        fullStr.substring(fullStr.length - backChars)
+    )
+}
+
+export default function NFTBox({ price, nftAddress, tokenId, marketplaceAddress, seller }) {
+    const [imageURI, setImageURI] = useState("")
+    const [tokenName, setTokenName] = useState("")
+    const [tokenDescription, setTokenDescription] = useState("")
+    const { isWeb3Enabled, account } = useMoralis()
+
+    const { runContractFunction: getTokenURI } = useWeb3Contract({
+        abi: nftAbi,
+        contractAddress: nftAddress,
+        functionName: "tokenURI",
+        params: {
+            tokenId: tokenId,
+        },
+    })
+
+    async function updateUI() {
+        const tokenURI = await getTokenURI()
+        console.log(tokenURI)
+        if (tokenURI) {
+            const requestURL = tokenURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            const tokenURIResponse = await (await fetch(requestURL)).json()
+            const imageURI = tokenURIResponse.image
+            const imageURIURL = imageURI.replace("ipfs://", "https://ipfs.io/ipfs/")
+            setImageURI(imageURIURL)
+            setTokenName(tokenURIResponse.name)
+            setTokenDescription(tokenURIResponse.description)
+        }
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const isOwnedByUser = seller === account || seller === undefined
+    const formattedSellerAddress = isOwnedByUser ? "You" : truncateStr(seller || "", 15)
+
+    return (
+        <div>
+            <div className="pr-4">
+                {imageURI ? (
+                    <Card title={tokenName} description={tokenDescription}>
+                        <div className="p-2">
+                            <div className="flex flex-col items-end gap-2">
+                                <div>#{tokenId}</div>
+                                <div className="italic text-sm">
+                                    Owned by {formattedSellerAddress}
+                                </div>
+                                <Image
+                                    loader={() => imageURI}
+                                    src={imageURI}
+                                    height="200"
+                                    width="200"
+                                />
+                                <div className="font-bold">
+                                    {ethers.utils.formatUnits(price, "ether")} ETH
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                ) : (
+                    <div>Loading...</div>
+                )}
+            </div>
+        </div>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,45 @@
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { useMoralisQuery, useMoralis } from "react-moralis"
+import NFTBox from "../components/NFTBox"
 
 export default function Home() {
-    return <div className={styles.container}></div>
+    const { isWeb3Enabled, account } = useMoralis()
+    const { data: listedNfts, isFetching: fetchingListedNft } = useMoralisQuery(
+        "ActiveItem",
+        (query) => query.limit(10).descending("tokenId")
+    )
+    console.log(listedNfts)
+    return (
+        <div className="container mx-auto">
+            <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+            <div className="flex flex-wrap">
+                {isWeb3Enabled ? (
+                    fetchingListedNft ? (
+                        <div>isLoading...</div>
+                    ) : (
+                        listedNfts.map((nft) => {
+                            console.log(nft.attributes)
+                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
+                                nft.attributes
+                            return (
+                                <div>
+                                    <NFTBox
+                                        price={price}
+                                        nftAddress={nftAddress}
+                                        tokenId={tokenId}
+                                        marketplaceAddress={marketplaceAddress}
+                                        seller={seller}
+                                        key={`${nftAddress}${tokenId}`}
+                                    />
+                                </div>
+                            )
+                        })
+                    )
+                ) : (
+                    <div>Web3 Currently Not Enabled</div>
+                )}
+            </div>
+        </div>
+    )
 }
